7b8d6d9231f253c03e0016e5149dc6a00ccc50d6
Sargot15 <carlos.fernandezvegue@gmail.com>
2025-01-26 11:35:53 +0100

    Create board and adjust card sizes to fit the board

---
+++ game/board/board.gd
@@ -0,0 +1,49 @@
+extends Control
+
+@export var card_scene: PackedScene 
+@export var max_cards: int = 50
+@export var card_separation: int = 4
+
+@onready var grid_container: GridContainer = $CardsGrid
+
+func _ready():
+	generate_cards(max_cards)
+	#generate_cards(randi_range(3, max_cards))
+
+func generate_cards(num_cards):
+
+	# add new cards
+	for i in range(num_cards):
+		var card = card_scene.instantiate()
+		grid_container.add_child(card)
+	
+	adjust_size(num_cards)
+
+
+func adjust_size(num_cards):
+	
+	# calculate the number of colums of the grid
+	var container_size_x = get_viewport().get_visible_rect().size.x
+	var container_size_y = get_viewport().get_visible_rect().size.y
+
+	var relation_col_row = container_size_x / container_size_y
+	
+	var columns = ceil(sqrt(num_cards * relation_col_row))
+	grid_container.columns = columns
+
+	# adjust card size
+	var card_size = floor((container_size_x - (columns * card_separation)) / columns)
+	
+	for card in grid_container.get_children():
+		card.custom_minimum_size = Vector2(card_size, card_size)
+		
+		
+func _notification(what):
+	# adjust size when the window size is changed
+	if what == NOTIFICATION_RESIZED:
+		if (grid_container != null):
+			adjust_size(max_cards)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
